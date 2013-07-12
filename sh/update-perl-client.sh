ccd892e9cdfad5e06741ed89cd5cb0692a0ef5af
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-07-12 10:59:21 +0900

    add module updator which follow for the develop branch of https://github.com/jubatus/jubatus

---
+++ sh/update-perl-client.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -x # 実行されたコマンドを表示する
+set -e # エラーがあったら諦めろ
+set -u # 未定義変数があるとエラー
+
+BASEDIR=`cd $(dirname $0); pwd`
+USER_ID=`/usr/bin/id -u`
+
+$BASEDIR/../sh/generate-perl-client.sh develop
+$BASEDIR/../bin/generate-jubatus-pm.pl
