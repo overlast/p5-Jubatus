39d258d32bcd538aa73a4af850f5d62bf065d75c
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-07-26 16:29:58 +0900

    fix to use the templates of pod of J::*::Types.pm

--- bin/insert_pod.pl
+++ bin/insert_pod.pl
@@ -23,54 +23,29 @@ sub main {
             if ((exists $subdir_path->{file}) && ($subdir_path->{file} =~ m|Client.pm|)){
                 my $module_name =  $subdir_path->{dir}->{dirs}->[-1];
                 my $module_path = join "/",@{$subdir_path->{dir}->{dirs}};
-                print Dump $module_path;
-                my $client_module_path = $module_path."/Client.pm";
-                my $is_replace = &replace_true_to_pod($module_name, $client_module_path, $pod_tmpl_dir);
-
-#                push @use_modules, "use ".$name_space.";";
-#                my $get_client = &get_client_getter($name_space, $module_name);
-#                push @get_clients, $get_client;
-#                push @module_names, $module_name;
+                {
+                    my $pod_type = "client";
+                    my $client_module_path = $module_path."/Client.pm";
+                    my $is_replace = &replace_true_to_pod($module_name, $client_module_path, $pod_tmpl_dir, $pod_type);
+                }
+                {
+                    my $pod_type = "types";
+                    my $client_module_path = $module_path."/Types.pm";
+                    my $is_replace = &replace_true_to_pod($module_name, $client_module_path, $pod_tmpl_dir, $pod_type);
+                }
             }
         }
     }
-
-=pod
-    my $use_auto_gen_module = join "\n", @use_modules;
-    my $get_client_from_auto_gen_module = join "\n", @get_clients;
-    my $get_client_from_auto_gen_module_using_parameter = &get_client_getter_using_parameter(\@module_names);
-    my $jubatus_pm_tmpl_path = $FindBin::Bin."/../jubatus_pm_tmpl";
-    my $jubatus_pm_path = $FindBin::Bin."/../lib/Jubatus.pm";
-    open my $in, "<:utf8", $jubatus_pm_tmpl_path;
-    open my $out, ">:utf8", $jubatus_pm_path;
-    while (my $line = <$in>) {
-        if ($line =~ m|#### USE ALL AUTO GENERATE MODULES ####|) {
-            print $out $use_auto_gen_module."\n";
-        }
-        elsif ($line =~ m|#### GET CLIENT FROM AUTO GENERATE MODULES ####|) {
-            print $out $get_client_from_auto_gen_module."\n";
-        }
-        elsif ($line =~ m|#### GET CLIENT FROM AUTO GENERATE MODULES USING PARAMETER ####|) {
-            print $out $get_client_from_auto_gen_module_using_parameter."\n";
-        }
-        else {
-            print $out $line;
-        }
-    }
-    close $out;
-    close $in;
-=cut
-
     return;
 }
 
 sub replace_true_to_pod {
-    my ($module_name, $client_module_path, $pod_tmpl_dir) = @_;
+    my ($module_name, $client_module_path, $pod_tmpl_dir, $pod_types) = @_;
     my $module_name_nc = $module_name;
     $module_name_nc =~ tr|[A-Z]|[a-z]|;
-
+    my $is_replaced = 0;
     my $tmp_client_module_path = $client_module_path.".tmp";
-    my $pod_path = $pod_tmpl_dir."/".$module_name_nc.".pod";
+    my $pod_path = $pod_tmpl_dir."/".$module_name_nc."_".$pod_types.".pod";
     if (-f $pod_path) {
         open my $pod_in, "<:utf8", $pod_path;
         open my $module_in, "<:utf8", $client_module_path;
@@ -93,7 +68,9 @@ sub replace_true_to_pod {
         close $module_in;
         close $module_out;
 
-        system("mv $tmp_client_module_path $client_module_path");
+        my $is_mv = system("mv $tmp_client_module_path $client_module_path");
+        $is_replaced = 1 if (0 == $is_mv);
+    } else {
     }
-    return 1;
+    return $is_replaced;
 }
