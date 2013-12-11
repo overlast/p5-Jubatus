ebe94fbe553c83e7381837f285a97b0e66c5edcc
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-12-11 23:56:40 +0900

    Write the first template of this module

---
+++ lib/Jubatus/Common.pm
@@ -0,0 +1,83 @@
+package Jubatus::Common;
+
+use strict;
+use warnings;
+use utf8;
+use autodie;
+
+use Jubatus::Common::Client;
+use Jubatus::Common::Datum;
+use Jubatus::Common::MessageStringGenerator;
+use Jubatus::Common::Types;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Jubatus::Common - Perl interface of Jubatus::Common
+
+=head1 SYNOPSIS
+
+    use Jubatus::Common;
+
+=head1 DESCRIPTION
+
+This module provide a interface of
+
+=head1 METHODS
+
+Jubatus::Common provide
+
+=head2 Constructors
+
+This constructors can die when invalid parameters are given.
+
+=head3 Jubatus::Common->new();
+
+=head1 FUNCTIONS
+
+=head3 get()
+
+=head1 SEE ALSO
+
+L<http://jubat.us/>
+L<https://github.com/jubatus>
+L<https://github.com/overlast/p5-Jubatus>
+
+=head1 LICENSE
+
+Copyright (C) 2013 by Toshinori Sato (@overlast).
+
+The MIT License (MIT)
+
+Copyright (c) <year> <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+=head1 AUTHOR
+
+Toshinori Sato (@overlast) E<lt>overlasting@gmail.comE<gt>
+
+=cut
