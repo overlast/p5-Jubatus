ba36006dee90043cc153cc571cb781205a18f3e2
Toshinori Sato (@overlast) <overlasting@gmail.com>
2014-01-15 21:00:39 +0900

    Add lines to use IDL versions and swap order of deletion process of working directory

--- sh/generate-perl-client.sh
+++ sh/generate-perl-client.sh
@@ -12,6 +12,7 @@ CLIENT_DIR="${BASE_DIR}/../generate"
 
 [ $# -eq 0 ] || JUBATUS_BRANCH="${1}"
 
+rm -rf "${JUBATUS_DIR}"
 git clone https://github.com/jubatus/jubatus.git "${JUBATUS_DIR}"
 pushd "${JUBATUS_DIR}"
 git checkout "${JUBATUS_BRANCH}"
@@ -24,11 +25,11 @@ for IDL in *.idl; do
   NAME_SPACE="$(basename "${IDL}" ".idl")"
   NAME_SPACE="${NAME_SPACE^}"
   PM_DIR=${CLIENT_DIR}/lib
-  /home/overlast/git/other/jubatus/tools/jenerator/src/jenerator -l perl -n "Jubatus::${NAME_SPACE}::" -o "${PM_DIR}" "${IDL}"
+  IDL_HASH=`git log -1 --format=%H -- ${IDL}`
+  IDL_VER=`git describe ${IDL_HASH}`
+  /home/overlast/git/other/jubatus/tools/jenerator/src/jenerator -l perl -n "Jubatus::${NAME_SPACE}::" -o "${PM_DIR}" "${IDL}" --idl-version ${IDL_VER}
 done
 popd
 
 mkdir -p ${BASE_DIR}/../lib/Jubatus/
 cp -rf  ${PM_DIR}/Jubatus/* ${BASE_DIR}/../lib/Jubatus/
-rm -rf "${JUBATUS_DIR}"
-rm -rf "${CLIENT_DIR}"
